e43be7121d2c892cd5ab0b0f24daf77724200531
Artem Illarionov <37085065+e-pirate@users.noreply.github.com>
2021-02-24 01:55:07 +0300

    Added cancellation handler to aftercheck

--- asynctest.py
+++ asynctest.py
@@ -13,10 +13,11 @@ tasks = [ 'light', 'co2', 'dummy' ]
 statedb = { 'light': { 'isPending': False }, 'co2': { 'isPending': False }, 'dummy': { 'isPending': False } }
 dispatcher_lock = False
 
+
 async def task_check(task):
     log = logging.getLogger("__main__") 
     duration = random.randint(0, 2000) / 1000
-    log.debug('Checking task: ' + task + ' (' + str(duration) + 'ms)')
+    log.debug('Checking task: ' + task + ' (' + str(duration) + 's) started')
     await asyncio.sleep(duration)
     log.debug('Checking of task ' + task + ' finished')
     statedb[task]['isPending'] = False
@@ -25,17 +26,24 @@ async def task_check(task):
     else:
         return True
 
+
 async def task_aftercheck(pending_tasks):
     log = logging.getLogger("__main__") 
     log.debug('Aftercheck got ' + str(len(pending_tasks)) + ' task(s) to await for')
-    results = []
-    results = await asyncio.shield(asyncio.gather(*pending_tasks))                                      # 'Gathered' coroutings should be 'shielded' to keep them from
-                                                                                                        # being terminated recursively by the canceled aftercheck
-    if True in results:
-        log.debug('All pending task checks finished, starting aftercheck: ' + str(results))
-        task_dispatcher(tasks, asyncio.current_task())
+    try:
+        results = []                                                                                    # Gathered coroutings should be shielded to keep them from
+        results = await asyncio.shield(asyncio.gather(*pending_tasks))                                  # being terminated recursively by the canceled aftercheck
+    except asyncio.CancelledError:
+        log.debug('Pending aftercheck canceled')
+    else:
+        if True in results:
+            log.debug('All pending task checks finished, starting aftercheck: ' + str(results))
+            task_dispatcher(tasks)
+        else:
+            log.debug('All pending task checks finished, no state changed')
 
-def task_dispatcher(tasks, aftercheck_task=None) -> int:
+
+def task_dispatcher(tasks) -> int:
     global dispatcher_lock
     log = logging.getLogger("__main__") 
 
@@ -52,6 +60,7 @@ def task_dispatcher(tasks, aftercheck_task=None) -> int:
         if t._coro.__name__ == 'task_check':
             pending_tasks.append(t)
 
+    """ Spawn check for all tasks that are not currently been checked """
     spawned_tasks = []
     for t in tasks:
         if statedb[t]['isPending']:
@@ -62,11 +71,11 @@ def task_dispatcher(tasks, aftercheck_task=None) -> int:
         spawned_tasks.append(new_task)
     log.debug('Dispatcher finished: ' + str(len(pending_tasks)) + ' task check(s) pending, ' + str(len(spawned_tasks)) + ' task check(s) spawned') #: ' + str(statedb))
 
-    if len(spawned_tasks) > 0:                                                                          # Lunch new aftercheck only if any new checks were spawned during this cycle
-        for t in asyncio.all_tasks():                                                                   # Cancel all pending afterchecks excluding new one
-            if t._coro.__name__ == 'task_aftercheck' and t != aftercheck_task:
+    """ Spawn trailing aftercheck if new task checks were schedulled """
+    if len(spawned_tasks) > 0:
+        for t in asyncio.all_tasks():                                                                   # Cancel all pending afterchecks, caller will be skipped
+            if t._coro.__name__ == 'task_aftercheck':
                 t.cancel()
-                log.debug('Pending aftercheck canceled')
 
         asyncio.create_task(task_aftercheck(pending_tasks + spawned_tasks))
 
@@ -83,6 +92,7 @@ async def tasks_loop(tasks):
 #async def main_loop(tasks):
 #    await asyncio.gather(tasks_loop(tasks), tasks_loop(tasks))
 
+
 def main():
     """ Setup logging """
     handler = logging.StreamHandler()
@@ -94,6 +104,7 @@ def main():
     log.info('Starting asyncio test program')
 
     asyncio.run(tasks_loop(tasks))
+
 
 if __name__ == "__main__":
     main()
